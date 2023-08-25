5f094f9d6e064a8073ef6d630ee98f82e838f687
shill <seth.hillsberry@gmail.com>
2023-08-24 20:49:22 -0500

    Initialized project using create react app
    Added EmailForm component that needs expanded on.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import EmailForm from './emailForm/EmailForm'
+
+function App() {
+  return (
+    <div className="App">
+      <EmailForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emailForm/EmailForm.js
@@ -0,0 +1,9 @@
+export default function(){
+	return(
+		<div className="email-form-container">
+			<form className="email-form">
+				<input type="email"/>
+			</form>
+		</div>
+);
+}
