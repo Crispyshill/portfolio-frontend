ccf38e9ed068c3307e18e2f6ce3ae44fe0a2e723
Seth Hillsberry <Shillsberry@protonmail.com>
2023-08-25 17:35:07 -0500

    Fleshed out form

--- src/emailForm/EmailForm.js
+++ src/emailForm/EmailForm.js
@@ -3,7 +3,12 @@ export default function(){
 		<div className="email-form-container">
 			<span>Email Form: </span>
 			<form className="email-form">
-				<input type="email"/>
+				<label for="email">Email</label>
+				<input id="email" type="email"/>
+				<label for="email-subject">Subject:</label>
+				<input id="email-subject" type="text"/>
+				<label for="email-body">Body:</label>
+				<textarea id="email-body"/>
 			</form>
 		</div>
 );
