d8c15cbff1a50024732a9db68c1d122a8af7eedc
Seth Hillsberry <Shillsberry@protonmail.com>
2023-08-28 17:28:51 -0500

    Created content changes on the about page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EmailForm from './shared/emailForm/EmailForm'
 import AboutPage from './aboutPage/AboutPage'
+import Navigation from './shared/Navigation'
 
 export default function App() {
   return (
     <div className="App">
-      <AboutPage/>
+      <Navigation/>
 	  <Routes>
         <Route path="/about" element={<AboutPage />} />
         <Route path="/services" element={<EmailForm />} />
--- src/aboutPage/AboutPage.js
+++ src/aboutPage/AboutPage.js
@@ -1,10 +1,13 @@
 import Navigation from '../shared/Navigation'
+import './AboutPage.css'; // Import the CSS file
 
 export default function AboutPage(){
 	return (
-		<div>
-			<Navigation/>
-			<span>Megan Baxter</span>
-		</div>
+		<div class="rectangle">
+        		<div class="header">Hello</div>
+        		<div class="content">
+            				My name is Megan Hillsberry and I am a 24 year old photographer based out of Boise, Idaho. I specialize in portraits, family photos, and promotional photography for many small businesses. Whether you're here to capture your family's heartfelt moments, or boost your business's image, I look forward to creating those lasting memories with you.
+        		</div>
+    		</div>
 	);
 }
