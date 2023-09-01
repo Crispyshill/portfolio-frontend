c93029cb1626c4b8907b6bffb0981d53ceabe98e
Seth Hillsberry <Shillsberry@protonmail.com>
2023-08-31 20:03:32 -0500

    Tried to fix css positioning on home page

--- src/home/Home.js
+++ src/home/Home.js
@@ -5,9 +5,12 @@ export default function Home(){
 	return (
 	<div className='home-container'>
 		<div className='tiger-banner-container'>
-		          <img src={tigerBanner} alt="tiger banner" className="tiger-banner"/>
+		<div className='tiger-banner'>
 			<div className='text-overlay'>
 			<span>Megan Hillsberry</span>
+		</div>
+
+		          <img src={tigerBanner} alt="tiger banner" className="tiger"/>
 		</div>
 		</div>
 		</div>
