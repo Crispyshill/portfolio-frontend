802504e567a9fe080715f7ef5c6dd5f1b098746e
Seth Hillsberry <Shillsberry@protonmail.com>
2023-09-02 12:45:46 -0500

    Added about section to home page

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,6 @@
 import tigerBanner from '../img/tiger_banner.JPG'
 import './Home.css';
+import aboutImage from '../img/about-image.png'
 
 export default function Home(){
 	return (
@@ -12,6 +13,16 @@ export default function Home(){
 			<h1>Megan Hillsberry</h1>
 		<span>Bringing your world into focus</span>
 	</div>
+		</div>
+		<div className='about'>
+		<div className='about-text'>
+		<h4>About</h4>
+		<span>Hillsberry Photography est. 2023 based in Boise, Idaho specializing in family and small business photography all at an affordable and adjustable price.</span>
+		<button>About Me</button>
+		</div>
+		<div className='about-image'>
+		<img src={aboutImage} alt="about image"/>
+		</div>
 		</div>
 		</div>
 	);
